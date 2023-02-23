d0cd2e081f8eaefc5cb1b8a8453a4628ca343deb
flenry <cjhlow002@mymail.sim.edu.sg>
2023-02-24 01:08:56 +0800

    Update: Improved Post

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import AddPost from "./components/AddPost";
+
 export default function Home() {
   return (
     <main>
       <h1 className="test-lg py-5">hello world</h1>
+      <AddPost />
     </main>
   );
 }
