346e55e262f335ffdb28d91f1eb9c6f403c84715
flenry <cjhlow002@mymail.sim.edu.sg>
2023-02-26 00:23:33 +0800

    Updated Posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,38 @@
 "use client";
 import AddPost from "./components/AddPost";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import Post from "./components/Post";
+
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
+  if (error) return error;
+  if (isLoading) {
+    return "Loading...";
+  }
+
+  console.log(data);
+
   return (
     <main>
-      <h1 className="test-lg py-5">hello world</h1>
       <AddPost />
+      {data?.map((post) => (
+        <Post
+          key={post.id}
+          name={post.user.name}
+          avatar={post.user.image}
+          postTitle={post.title}
+          id={post.id}
+        />
+      ))}
     </main>
   );
 }
